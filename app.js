d759656a5874f73dba8c2d4525de5abd9d89d200
ailynVG <110043075+ailynVG@users.noreply.github.com>
2022-10-25 19:44:55 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');//
+var connection = mysql.createConnection({//Crear conexión del archivo con la base de datos
+   host: 'localhost',//local
+   user: 'root',//usuario
+   password: '',//contraseña
+   database: 'mitzi'//Nombre Base de Datos
+});
+connection.connect(function(error){
+   if(error){
+      throw error;
+   }else{
+      console.log('Conexion exitosa.');
+   }
+});
+conexion.query('SELECT * FROM usuario', function(error, results, fields){
+    if(error)
+    throw error;
+    results.forEach (result =>{
+        console.log(result);
+    });
+})
+connection.end();
